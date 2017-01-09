252c0c80750811c450d96f17a4f4272e99b4bbc7
xinghc <xinghc@gmx.us>
2017-01-10 00:31:40 +0800

    list2-4: quantile-quantile figure

---
+++ ch2/qqplotAttribute.py
@@ -0,0 +1,35 @@
+__author__ = 'mike_bowles'
+import numpy as np
+import pylab
+import scipy.stats as stats
+import urllib2
+import sys
+
+target_url = ( "https://archive.ics.uci.edu/ml/machine-learning-databases/undocumented/connectionist-bench/sonar/sonar.all-data" )
+data = urllib2.urlopen(target_url)
+
+xList = []
+labels = []
+
+for line in data:
+        row = line.strip().split( "," )
+        xList.append(row)
+nrow = len(xList)
+ncol = len(xList[1])
+
+type_data = [0]*3
+colCounts = []
+
+col = 3
+colData = []
+for row in xList:
+        colData.append(float(row[col]))
+
+colArray = np.array(colData)
+colMean = np.mean(colArray)
+colStd = np.std(colData)
+
+sys.stdout.write("Mean = " + str(colMean) + "\tStandard Deviation = " + str(colStd) + "\n")
+
+stats.probplot(colData, dist="norm", plot=pylab)
+pylab.show()
