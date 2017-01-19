b7fd326f5253a767d06ab20b9a0d69e3a1e556c2
xinghc <xinghc@gmx.us>
2017-01-20 00:59:27 +0800

    list2-8: chose V35 to find mines

---
+++ ch2/targetCorr.py
@@ -0,0 +1,35 @@
+__author__ = 'mike_bowles'
+import pandas as pd
+from pandas import DataFrame
+import matplotlib.pyplot as plot
+from random import uniform
+
+target_url = ( "https://archive.ics.uci.edu/ml/machine-learning-databases/undocumented/connectionist-bench/sonar/sonar.all-data" )
+
+rocksVMines = pd.read_csv(target_url, header=None, prefix=" V")
+
+target = []
+for i in range(208):
+	if rocksVMines.iat[i, 60] == "M":
+		target.append(1.0)
+	else:
+		target.append(0.0)
+
+dataRow = rocksVMines.iloc[0:208, 35]
+plot.scatter(dataRow, target)
+plot.xlabel( "Attribute Value" )
+plot.ylabel( "Target Value" )
+plot.show()
+
+target = []
+for i in range(208):
+	if rocksVMines.iat[i, 60] == "M":
+		target.append(1.0 + uniform(-0.1, 0.1))
+	else:
+		target.append(0.0 + uniform(-0.1, 0.1))
+
+dataRow = rocksVMines.iloc[0:208, 35]
+plot.scatter(dataRow, target, alpha=0.5, s=120)
+plot.xlabel( "Attribute Value" )
+plot.ylabel( "Target Value" )
+plot.show()
