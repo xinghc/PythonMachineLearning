887dcf2d0efa6552e9df4fcce6278e1d3de9ef9a
xinghc <xinghc@gmx.us>
2017-02-04 01:14:57 +0800

    list2-12: Parallel coordinates plot with logit normalized

---
+++ ch2/abaloneParallePlot.py
@@ -0,0 +1,40 @@
+__author__ = 'mike_bowles'
+import pandas as pd
+from pandas import DataFrame
+import matplotlib.pyplot as plot
+from math import exp
+
+target_url = \
+	("http://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data")
+
+abalone = pd.read_csv(target_url,header=None, prefix="V")
+abalone.columns = ['Sex', 'Length', 'Diameter', 'Height',
+                   'Whole Wt', 'Shucked Wt',
+                   'Viscera Wt', 'Shell Wt', 'Rings']
+
+summary = abalone.describe()
+minRings = summary.iloc[3, 7]
+maxRings = summary.iloc[7, 7]
+nrows = len(abalone.index)
+
+for i in range(nrows):
+    dataRow = abalone.iloc[i, 1:8]
+    labelColor = (abalone.iloc[i, 8] - minRings) / (maxRings - minRings)
+    dataRow.plot(color=plot.cm.RdYlBu(labelColor), alpha=0.5)
+
+plot.xlabel("Attribute Index")
+plot.ylabel(("Attribute Values"))
+plot.show()
+
+meanRings = summary.iloc[1,7]
+sdRings = summary.iloc[2,7]
+
+for i in range(nrows):
+    dataRow = abalone.iloc[i,1:8]
+    normTarget = (abalone.iloc[i,8] - meanRings)/sdRings
+    labelColor = 1.0/(1.0 + exp(-normTarget))
+    dataRow.plot(color=plot.cm.RdYlBu(labelColor), alpha=0.5)
+
+plot.xlabel("Attribute Index")
+plot.ylabel(("Attribute Values"))
+plot.show()
